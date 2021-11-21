f04530c0cbf0321977882be294aaf82f36f0c379
Jb503 <jb-503@hotmail.com>
2021-11-20 17:37:56 -0800

    fixed field

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Blue_Right.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Blue_Right.java
@@ -10,25 +10,25 @@ public class Auto_Blue_Right extends DriveCodeCommon {
         robot.init(hardwareMap);
         ResetWheelEncoders();
         waitForStart();
-        verticalDrive(-5,0.3);
+        verticalDrive(-5,0.5);
         robot.PivotMotor.setTargetPosition(Raised);
         robot.PivotMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         robot.PivotMotor.setPower(0.3);
         while (robot.PivotMotor.isBusy()){}
         robot.PivotMotor.setPower(0);
-        horizontalDrive(-23,0.5);
+        horizontalDrive(-23,0.3);
         robot.PivotMotor.setTargetPosition(Scoring);
         robot.PivotMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         robot.PivotMotor.setPower(0.3);
         while (robot.PivotMotor.isBusy()){}
         robot.PivotMotor.setPower(0);
-        verticalDrive(-4,0.5);
+        verticalDrive(-12,0.3);
         ScoreMid();
         sleep(2000);
         HoldMid();
         HighHold();
-        horizontalDrive(-51, 0.5);
-        verticalDrive(-19,0.5);
+        horizontalDrive(-51, 0.3);
+        verticalDrive(-10,0.3);
         robot.PivotMotor.setTargetPosition(Intake);
         robot.PivotMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         robot.PivotMotor.setPower(0.3);
